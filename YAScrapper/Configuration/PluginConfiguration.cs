37396c3a1ac3f824a528eb1a75d5558b9f88cb55
DoucheHellSung <pcreamer@list.ru>
2022-03-19 13:07:54 +0530

    Add project files.

---
+++ YAScrapper/Configuration/PluginConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaBrowser.Model.Plugins;
+namespace YAScrapper.Configuration
+{
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+       
+    }
+}
---
+++ YAScrapper/Plugin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using YAScrapper.Configuration;
+namespace YAScrapper
+{
+    internal class Plugin : BasePlugin<PluginConfiguration>
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+
+        }
+        
+
+
+        public static string PluginName = "YAScrapper";
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = "YAScrapper",
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
+                }
+            };
+        }
+        private Guid _id = new Guid("EF3B097A-C1A7-457E-BC96-DE916C16D568");
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+
+       
+        /// <summary>
+        /// Gets the name of the plugin
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get { return PluginName; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public override string Description
+        {
+            get
+            {
+                return "Plugin to crawl websites.";
+            }
+        }
+
+        // public string PluginName { get; private set; }
+        public static Plugin Instance { get; private set; }
+    }
+}
